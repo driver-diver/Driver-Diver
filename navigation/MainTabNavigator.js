96d90bb2775a339a9857b0c4f7d285c72cfe0b39
jcriseno <criseno@sonoma.edu>
2019-04-05 15:21:16 -0700

    Added-NewTab-With-Maps

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,6 +6,25 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import MapScreen from '../screens/MapScreen';
+
+const MapStack = createStackNavigator({
+    Maps: MapScreen,
+});
+
+MapStack.navigationOptions = {
+    tabBarLabel: 'Maps',
+    tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+            focused={focused}
+            name={
+                Platform.OS === 'ios'
+                    ? `ios-information-circle${focused ? '' : '-outline'}`
+                    : 'md-information-circle'
+            }
+        />
+    ),
+};
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -57,4 +76,5 @@ export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
+    MapStack,
 });
