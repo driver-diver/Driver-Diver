33b869440ae2690a8141ffda0726e9342b3558eb
jcriseno <criseno@sonoma.edu>
2019-04-05 17:35:56 -0700

    Added-Splash-and-FinishedMaps

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,19 +3,18 @@ import { Platform, Text, View, StyleSheet } from 'react-native';
 import { Constants, Location, Permissions, MapView } from 'expo';
 
 export default class App extends React.Component {
-    state = {
-        location: null,
-        errorMessage: null,
-    };
 
-    componentWillMount() {
-        if (Platform.OS === 'android' && !Constants.isDevice) {
-            this.setState({
-                errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-            });
-        } else {
-            this._getLocationAsync();
-        }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            region: {
+                latitude: 0,
+                longitude: 0
+            },
+            errorMessage: null,
+
+        };
     }
 
     _getLocationAsync = async () => {
@@ -28,8 +27,21 @@ export default class App extends React.Component {
 
         let location = await Location.getCurrentPositionAsync({});
         console.log(location);
+        //this.setState({ region: {latitude: location.coords.latitude, longitude: longitude.coords.longitude} });
         this.setState({ location });
     };
+
+    componentWillMount() {
+        if (Platform.OS === 'android' && !Constants.isDevice) {
+            this.setState({
+                errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+            });
+        } else {
+            this._getLocationAsync();
+        }
+    }
+
+
     /*
     render() {
         let text = 'Waiting..';
@@ -49,18 +61,25 @@ export default class App extends React.Component {
 
     render() {
         return (
-
             <MapView
                 style={{ flex: 1 }}
                 initialRegion={{
-
                     latitude: 37.78825,
                     longitude: -122.4324,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
-
+                }}
+                showsUserLocation={true}
+                region={{
+                    //latitude: this.state.region.coords.latitude,
+                    //longitude: this.state.region.coords.longitude,
+                    latitude: 38.3386,
+                    longitude: -122.6748,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
                 }}
             />
+
         );
     }
 }
